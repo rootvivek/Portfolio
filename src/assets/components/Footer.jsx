7bdb626776535943d9f7dc389634aae8e4518f88
rootvivek <rootvivek@gmail.com>
2022-05-02 09:44:27 +0530

    commit

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Footer() {
   return (
@@ -9,9 +7,6 @@ function Footer() {
             <div className="row">
                 <div className="col">
                     <h2 className="title py-5">Let's Connect</h2>
-                    <FontAwesomeIcon icon={solid('user-secret')} />
-                    <FontAwesomeIcon icon={regular('coffee')} />
-                    <FontAwesomeIcon icon={brands('twitter')} />
                 </div>
             </div>
         </div>
