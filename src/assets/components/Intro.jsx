5230b249ad025006697550d003b34f557eddd638
Vivek Pal <rootvivek@gmail.com>
2022-09-08 12:26:34 +0530

    new

--- src/assets/components/Intro.jsx
+++ src/assets/components/Intro.jsx
@@ -6,27 +6,27 @@ import Navbar from './Navbar';
 function Intro() {
   return (
 <div className="gradient-background">
-    <div className ="container">
-         <Navbar/>
+    <div className ="container pt-4">
+        <Navbar/>
         <div className ="row">
             <div className="col-lg-12">
                 <div className="intro">
-                    <div className="intro-left col-lg-7">
-                        <h3 className="text-light">Namaste 🙏</h3>
-                        <h2 className="d-flex intro-name text-light">I' m
+                    <div className="intro-left col-lg-6">
+                        <h1 className="text-light">Namaste 🙏</h1>
+                        <h1 className="d-flex intro-name text-light pb-1">I' m {" "}
                         <Typewriter className="dynamic-text"
                             options={{
-                                strings: ['Vimal Kumar.','User Interface Designer.','Frontend Engineer.'],
+                                strings: ['Vimal Kumar.','UI Designer.','Frontend Engineer.'],
                                 autoStart: true,
                                 loop: true,
                                 pauseFor: 3000,
                             }}
                             />
-                        </h2>
-                        <h6 className="intro-subtitle text-light mt-3">A Designer who write codes. i like to make digital experiences easier and simpler for people.</h6>
+                        </h1>
+                        <h4 className="intro-subtitle text-light mt-3">A Designer who write codes. i like to make digital experiences easier and simpler for people.</h4>
                     </div>
                     <div className="intro-right col-lg-5">
-                        <img src={intro} className="intro-img" alt='intro-img' />
+                        {/* <img src={intro} className="intro-img" alt='intro-img' /> */}
                     </div>
                 </div>
             </div>
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className ="navbar navbar-expand-md bg-faded navbar-light pt-4">
+    <nav className ="navbar navbar-expand-lg navbar-light">
         <div className ="container">
-          {/* <NavLink className ="navbar-brand py-3 h1" to={""}>Vimal Kumar</NavLink> */}
+          <NavLink className ="navbar-brand h1 mb-lg-0 ps-3 text-light" to={""}>Vimal Kumar</NavLink>
             <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className ="navbar-toggler-icon"></span>
             </button>
-          <div className ="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
-            <div className="navbar-area">
-              <ul className ="navbar-nav mb-2 mb-lg-0">
+          <div className ="collapse navbar-collapse d-flex" id="navbarSupportedContent">
+            <div className="navbar-area ms-auto">
+              <ul className ="navbar-nav me-auto mb-lg-0">
                 <li className ="nav-item">
                   <Link className ="nav-link active" aria-current="page" to='/'>Home</Link>
                 </li>
