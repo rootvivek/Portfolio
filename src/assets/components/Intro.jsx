017a53d3f52e10c467b3adfd7c9cef934c037d3a
Vivek Pal <rootvivek@gmail.com>
2022-09-09 00:25:12 +0530

    ser

--- src/assets/components/Intro.jsx
+++ src/assets/components/Intro.jsx
@@ -11,7 +11,7 @@ function Intro() {
             <div className="col-lg-12">
                 <div className="intro pb-5">
                     <div className="intro-left col-lg-7">
-                        <h1 className="intro-text">Hello,I' m Vimal.</h1>
+                        <h1 className="intro-text">Hello, I'm Vimal.</h1>
                         <h1 className="intro-text">I'm a designer.</h1>
                         <h5 className="intro-subtitle">A Designer who write codes. i like to make digital experiences easier and simpler for people.</h5>
                     </div>
