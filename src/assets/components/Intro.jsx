7c82de76ee867edfd7575c45f2f56b93d75e3e63
Vivek Pal <rootvivek@gmail.com>
2022-10-09 23:07:14 +0530

    Update Intro.jsx

--- src/assets/components/Intro.jsx
+++ src/assets/components/Intro.jsx
@@ -13,7 +13,7 @@ function Intro() {
             <div className="col-12">
                 <div className="intro">
                         <h1 className="intro-text">Hello, I'm Vimal.</h1>
-                        <h1 className="intro-text">UI Designer and Frontend Developer.</h1>
+                        <h1 className="intro-text">UI Designer and Frontend Web Developer.</h1>
                         <h5 className="intro-subtitle">I design and code beautifully simple things, and I love what I do.</h5>
                         <img className='intro-avatar' src = {vk} alt="#"></img>
                     </div>
