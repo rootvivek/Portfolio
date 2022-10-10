b4831d83fb28b961e38534fa9477343067e50ba2
Vivek Pal <rootvivek@gmail.com>
2022-10-10 08:04:10 +0530

    home

--- src/assets/components/CardMain.jsx
+++ src/assets/components/CardMain.jsx
@@ -7,7 +7,7 @@ function CardMain() {
        <div className="project">
           <div className="container overflow-hidden">
                 <div className="row">                   
-                        <h1 className="title ps-3">Selected Works</h1>                             
+                        <h1 className="title ps-3">Explore my best projects</h1>                             
                           <Card details = {CardData}/>
                     </div>
                 </div>
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className ="navbar navbar-expand-lg navbar-light">
         <div className ="container">
-          <NavLink className ="navbar-brand ps-3 text-dark" to={""}>VK.</NavLink>
+          <NavLink className ="navbar-brand ps-3 text-dark" to='/'>VK.</NavLink>
             <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className ="navbar-toggler-icon"></span>
             </button>
