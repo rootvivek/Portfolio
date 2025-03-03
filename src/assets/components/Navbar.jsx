89ad6cca404121b38f500f50aae06f5a4f883bee
Vivek Pal <rootvivek@gmail.com>
2025-03-03 11:32:30 +0530

    Update Navbar.jsx

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link,NavLink } from 'react-router-dom'
-import vk_resume from "../resume.pdf";
+import vk_resume from "../Resume.pdf";
 
 function Navbar() {
   return (
